bef24b3b9018ba1979af8ad3cfd754f63e63dcbf
Asya Haroyan <alharoyan@gmail.com>
2024-04-27 08:49:49 +0000

    Chnage secret key

--- main/settings.py
+++ main/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*f($++7y7g4o!$i^wus!t5mxz9*w_ro*kf_0uxxc^c@#h^6$7('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-asyaharoyan-brainondjan-tjxpxcxtanx.ws-eu110.gitpod.io',
-'.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-asyaharoyan-brainondjan-tjxpxcxtanx.ws-eu110.gitpod.io',
+    '.herokuapp.com'
+]
 
 
 # Application definition
