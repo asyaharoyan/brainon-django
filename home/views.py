b14cf55aa03aee2b5436df35f3c8ee6c0b732450
Asya Haroyan <alharoyan@gmail.com>
2024-04-27 09:42:31 +0000

    Create basic home view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome!")
+class Index(TemplateView):
+    template_name = 'home/index.html'
